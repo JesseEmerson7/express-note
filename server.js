6500d9438e68bbf3b61258e63ea9d38f53451b11
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-27 18:45:51 -0400

    added some padding to note title input on bottom

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-
+//imported modules
 const fs = require("fs/promises");
 const express = require("express");
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const api = require('./routes/api-notes')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//middleware
 app.use(express.json());
 app.use(express.static("public"));
 app.use(logger);
 app.use("/api", api);
 
+//HomePage routing and notes page. *all api/routes methods are in routes/api-notes.js file*
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
