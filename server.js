7626be5153c73d73d87d04f49b91b4d103f274af
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-27 07:57:00 -0400

    functioning delete button on notes.html connecting to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//TO DO: (1)finnish delete method. (2)set up routs file. (3)deploy to heroku. (4)write readme. 
+//TO DO: (1)finnish delete method. (2)set up routs file. (3)deploy to heroku. (4)write readme.
 
 const fs = require("fs/promises");
 const express = require("express");
@@ -43,7 +43,7 @@ app.post("/api/notes", async (req, res) => {
     const pastTipsJson = JSON.stringify(pastTipsArray);
 
     await fs.writeFile("./db/db.json", pastTipsJson, { flag: "w" });
-
+    console.log("Tip posted!");
     res.sendStatus(200);
   } catch (err) {
     console.log(err);
@@ -51,8 +51,21 @@ app.post("/api/notes", async (req, res) => {
   }
 });
 
-//where I left off trying to delete
-app.delete("/api/notes/:id", async (req, res) => {});
+app.delete("/api/notes/:id", async (req, res) => {
+  try {
+    let deleteId = req.params.id;
+    const data = await fs.readFile("./db/db.json");
+    const pastTipsArray = JSON.parse(data);
+    const deletedIndex = pastTipsArray.findIndex((tip) => tip.id === deleteId);
+    pastTipsArray.splice(deletedIndex, 1);
+    fs.writeFile("./db/db.json", JSON.stringify(pastTipsArray), { flag: "w" });
+    console.log(`Post deleted!`);
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
